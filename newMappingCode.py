4e30fd33bd08455cb9c199f4142dcfe6997f8938
hall593 <72887646+hall593@users.noreply.github.com>
2021-04-21 17:24:26 -0400

    Update newMappingCode.py

--- newMappingCode.py
+++ newMappingCode.py
@@ -1,75 +1,61 @@
 from MPU9250 import MPU9250
-import time
-import math
+import sys
+import time # import time library for sleep and timer function
 import brickpi3 # import the BrickPi3 drivers
-
-mpu9250 = MPU9250()
+import numpy # hopefully numpy is here
 
 BP = brickpi3.BrickPi3()
-BP.set_sensor_type(BP.PORT_1, BP.SENSOR_TYPE.TOUCH)
 
-tFLAG=0
-edgeNew=0
-edgeOld=0
-fid = open("FieldMagData.csv","w")
-fid2 = open("FieldVecData.csv","w")
-fid.write("Points,")
-fid2.write("Points,")
+timeStart = time.time()
+dT = 3 # 3 seconds to next box?
+timeRunning = 0
 
-xwidth=int(input("Enter the total number of x steps: "))
-ywidth=int(input("Enter the total number of y steps: "))
 
-for q in range(0,xwidth+1):
-    fid.write(str(q))
-    fid.write(",")
-    fid2.write(str(q))
-    fid2.write(",")
-    fid2.write(",")
-    fid2.write(",")
+#mazeMap = numpy.zeros(shape = (7,7))
+
+mazeMap = [[ 0, 0, 0, 0, 0, 0, 0], [ 0, 0, 0, 0, 0, 0, 0], [ 0, 0, 0, 0, 0, 0, 0], [ 0, 0, 0, 0, 0, 0, 0], [ 0, 0, 0, 0, 0, 0, 0], [ 0, 0, 0, 0, 0, 0, 0], [ 0, 0, 0, 0, 0, 0, 0]]
+X = 3 # starting x point
+Y = 0 # starting y point
+
+mazeMap[Y][X] = 5
+
+def markMap(occurrence): # changes number based on occurrence key (IR, magnet, nothing)
+    if(occurrence == 4): # case of IR detection
+        mazeMap[Y][X] = 4
+    else if (occurrence == 3): # case of strong magnetic reading
+        mazeMap[Y][X] = 3
+    else: # normal readings
+        mazeMap[Y][X] = 1
+
+def moveLeft():
+    X += 1
     
-fid.write("\n")
-fid2.write("\n")
-
-time.sleep(0.1)
-
+def moveRight():
+    X -= 1
+    
+def moveForward():
+    Y += 1
+    
+def move Backwards():
+    Y -= 1
+    
+def checkTurns(orientation): # return value of x, y to the left and right of robot
+    if(orientation == 1): # positioned forward relative to starting pt
+        if(mazeMap[Y][X + 1] == 1): # left of the robot has been navigated?? DEPENDS ON ORIENTATION, this is on oriented forward, make a variable that tracks orientation values 1 - 4
+            return 1 # not sure about this yet
+        else if(mazeMap[Y][X - 1] == 1): # right of robot
+            return 2
+    if(orientation == 2): # positioned turned horizontal relative to start
+        if(mazeMap[Y + 1][X] == 1): # right has been navigated if robot is positioned to the left
+            return 1
+        else if(mazeMap[Y - 1][X] == 1): # spot between robot and x-axis
+            return 2
+        
 try:
-    for x in range(0,xwidth):
-        fid.write(str(x))
-        fid.write(",")
-        fid2.write(str(x))
-        fid2.write(",")
-        for y in range(0,ywidth):
-            print("Move to location ("+str(x)+" , "+str(y)+") and press the button")
-            while not tFLAG:
-                try:
-                    edgeNew = BP.get_sensor(BP.PORT_1)
-                    if edgeOld==1 and edgeNew==0:
-                        tFLAG=1
-                    edgeOld=edgeNew
-                except brickpi3.SensorError as error:
-                    print(error)
-            try:
-                mag = mpu9250.readMagnet()
-                total_mag=math.sqrt(mag['x']*mag['x']+mag['y']*mag['y']+mag['z']*mag['z'])
-                fid.write(str(total_mag))
-                fid.write(",")
-                fid2.write(str(mag['x']))
-                fid2.write(",")
-                fid2.write(str(mag['y']))
-                fid2.write(",")
-                fid2.write(str(mag['z']))
-                fid2.write(",")
-            except IOError:
-                print ("IO Error")
-            tFLAG=0
-        fid.write("\n")
-        fid2.write("\n")
-    fid.close()
-    fid2.close()
-    print("Data Collection Completed.\n")
-
-
-except KeyboardInterrupt:
+    while True:
+        while(time.time() - timeStart < 3):
+          
+        
+except KeyboardInterrupt: # except the program gets interrupted by Ctrl+C on the keyboard.
+    print(mazeMap[0], "\n", mazeMap[1], "\n", mazeMap[2], "\n", mazeMap[3], "\n", mazeMap[4], "\n", mazeMap[5], "\n", mazeMap[6], "\n")
     BP.reset_all()
-    fid.close()
-    fid2.close()
